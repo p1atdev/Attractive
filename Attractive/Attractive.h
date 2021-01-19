c1827a9d85d97c7031e4a5980a1b287342d8c1a3
Shu Teiei <shu_teiei@outlook.jp>
2021-01-19 21:31:03 +0900

    Initial Commit

---
+++ Attractive/Attractive.h
@@ -0,0 +1,18 @@
+//
+//  Attractive.h
+//  Attractive
+//
+//  Created by 周廷叡 on 2021/01/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Attractive.
+FOUNDATION_EXPORT double AttractiveVersionNumber;
+
+//! Project version string for Attractive.
+FOUNDATION_EXPORT const unsigned char AttractiveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Attractive/PublicHeader.h>
+
+
